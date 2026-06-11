dbda27f39f59a882ef22f1b59ba7b2231c5349a4
EduKaique <eduardoalberico18@gmail.com>
2026-06-10 21:40:15 -0300

    feat: adiciona dados do curriculo atualizado

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,18 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
 export default defineConfig({
-  site: 'https://edukaique.github.io',
-  base: '/ds881-curriculo-GRR20240555',
-  integrations: [mdx(), tailwind()]
+  site: "https://EduKaique.github.io",
+  // base: '/ds881-curriculo-GRR20240555',
+  vite: {
+    server: {
+      watch: {
+        usePolling: true,
+        interval: 500,
+      },
+    },
+  },
+  integrations: [mdx(), sitemap(), tailwind()],
 });
